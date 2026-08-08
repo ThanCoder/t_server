c3345ab65b412350045b7df08e9680d6ea294754
ThanCoder <thancoder2019@gmail.com>
2026-08-08 11:40:36 +0630

    update

--- lib/core/t_server_base.dart
+++ lib/core/t_server_base.dart
@@ -51,7 +51,46 @@ class TServer {
 
   InternetAddress? get getAddress => _server?.address;
 
+  bool _isOpened = false;
+  bool get isOpened => _isOpened;
+
   /// ### Start Server
+  /// Starts listening for HTTP requests on the specified [address] and
+  /// [port].
+  ///
+  /// The [address] can either be a [String] or an
+  /// [InternetAddress]. If [address] is a [String], [bind] will
+  /// perform a [InternetAddress.lookup] and use the first value in the
+  /// list. To listen on the loopback adapter, which will allow only
+  /// incoming connections from the local host, use the value
+  /// [InternetAddress.loopbackIPv4] or
+  /// [InternetAddress.loopbackIPv6]. To allow for incoming
+  /// connection from the network use either one of the values
+  /// [InternetAddress.anyIPv4] or [InternetAddress.anyIPv6] to
+  /// bind to all interfaces or the IP address of a specific interface.
+  ///
+  /// If an IP version 6 (IPv6) address is used, both IP version 6
+  /// (IPv6) and version 4 (IPv4) connections will be accepted. To
+  /// restrict this to version 6 (IPv6) only, use [v6Only] to set
+  /// version 6 only. However, if the address is
+  /// [InternetAddress.loopbackIPv6], only IP version 6 (IPv6) connections
+  /// will be accepted.
+  ///
+  /// If [port] has the value 0 an ephemeral port will be chosen by
+  /// the system. The actual port used can be retrieved using the
+  /// [port] getter.
+  ///
+  /// The optional argument [backlog] can be used to specify the listen
+  /// backlog for the underlying OS listen setup. If [backlog] has the
+  /// value of 0 (the default) a reasonable value will be chosen by
+  /// the system.
+  ///
+  /// The optional argument [shared] specifies whether additional `HttpServer`
+  /// objects can bind to the same combination of `address`, `port` and `v6Only`.
+  /// If `shared` is `true` and more `HttpServer`s from this isolate or other
+  /// isolates are bound to the port, then the incoming connections will be
+  /// distributed among all the bound `HttpServer`s. Connections can be
+  /// distributed over multiple isolates this way.
   Future<void> start({
     String address = 'localhost',
     int port = 8080,
@@ -59,8 +98,31 @@ class TServer {
     bool v6Only = false,
     bool shared = false,
   }) async {
-    _server = await HttpServer.bind(address, port);
+    if (isOpened) return;
+    _server = await HttpServer.bind(
+      address,
+      port,
+      backlog: backlog,
+      v6Only: v6Only,
+      shared: shared,
+    );
     _server!.listen(_handleRequest);
+    _isOpened = true;
+  }
+
+  /// ### Stop Server
+  ///
+  /// Permanently stops this [HttpServer] from listening for new
+  /// connections.  This closes the [Stream] of [HttpRequest]s with a
+  /// done event. The returned future completes when the server is
+  /// stopped. For a server started using [bind] or [bindSecure] this
+  /// means that the port listened on no longer in use.
+  ///
+  /// If [force] is `true`, active connections will be closed immediately.
+  Future<void> stop({bool force = false}) async {
+    if (!_isOpened) return;
+    await _server?.close(force: force);
+    _server = null;
   }
   //****************Http Request Handler*********************** */
 
